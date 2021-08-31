8c6608149df6d656f8a3a8f56fcb1b7fadaaeb12
igniti0n <ivanat735@gmail.com>
2021-08-31 18:39:42 +0200

    initial setup

---
+++ Weather App/UI/Coordinators/RootCoordinator/RootCoordinator.swift
@@ -0,0 +1,21 @@
+//
+//  RootCoordinator.swift
+//  Weather App
+//
+//  Created by Ivan Stajcer on 31.08.2021..
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class RootCoordinator : Coordinator {
+    
+    var navigationController = UINavigationController()
+    
+    func start() -> UIViewController {
+        <#code#>
+    }
+    
+    
+}
